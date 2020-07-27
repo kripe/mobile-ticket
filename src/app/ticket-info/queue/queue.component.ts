2843d641ef2e3cae820b559ca99018cde8267189
Peter Kristensson <peter.kristensson@qmatic.com>
2020-07-27 11:42:42 +0200

    QP-8218 : MobileTicket slow response : Adjusted queue polling to not start a new poll when there is one already ongoing.

--- src/app/ticket-info/queue/queue.component.ts
+++ src/app/ticket-info/queue/queue.component.ts
@@ -145,16 +145,17 @@ export class QueueComponent implements OnInit, OnDestroy {
 
   public initPollTimer(visitPosition, ticketService: TicketInfoService) {
     if (visitPosition > 5) {
-      this.timer = TimerObservable.create(0, 5000);
+      this.timer = TimerObservable.create(1000, 5000);
     }
     if (visitPosition <= 5) {
-      this.timer = TimerObservable.create(0, 1000);
+      this.timer = TimerObservable.create(1000, 1000);
     }
     this.subscription = this.timer.subscribe(visitPosition => this.queuePoll(visitPosition, ticketService, false));
 
   }
 
   public queuePoll(visitPosition, ticketService: TicketInfoService, onRetry: boolean) {
+    this.doUnsubscribeForPolling();
     ticketService.pollVisitStatus((queueInfo: QueueEntity) => {
       this.doSubscribeForPolling();
       this.onQueuePollSuccess(queueInfo, ticketService);
